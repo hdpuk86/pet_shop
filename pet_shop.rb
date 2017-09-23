0a028cd0e1faa9cd99cdbd9dd8c65b96bc0fff34
hdpuk86 <hayley.d.prior@gmail.com>
2017-09-23 22:22:19 +0100

    refine functions

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,10 +1,10 @@
 
 def pet_shop_name(shop)
-  return shop[:name]
+  shop[:name]
 end
 
 def total_cash(shop)
-  return shop[:admin][:total_cash]
+  shop[:admin][:total_cash]
 end
 
 def add_or_remove_cash(shop, amount)
@@ -12,7 +12,7 @@ def add_or_remove_cash(shop, amount)
 end
 
 def pets_sold(shop)
-  return shop[:admin][:pets_sold]
+  shop[:admin][:pets_sold]
 end
 
 def increase_pets_sold(shop, amount)
@@ -20,7 +20,7 @@ def increase_pets_sold(shop, amount)
 end
 
 def stock_count(shop)
-  return shop[:pets].length()
+  shop[:pets].length()
 end
 
 def pets_by_breed(shop, breed)
